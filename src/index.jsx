cf482a5e4d080f23d7dc1b9b5cfb305f9463505d
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-27 17:01:11 -0600

    [finish] finish set up for react

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import {render} from 'react-dom'
+
+const root = document.getElementById('root')
+
+render(<h1>I'm alive</h1>, root)
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const babelRules = {
+  test: /\.(js|jsx)$/,
+  loader: 'babel-loader',
+  exclude: /node_modules/,
+};
+
+module.exports = {
+  entry: ['react-hot-loader/patch', './src/index.jsx'],
+  module: {
+    rules: [babelRules],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
