d4af788ce2c164d5fade879e69d49162441adeea
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-27 21:50:27 -0600

    [update] add webpack config for dev mode

--- README.md
+++
@@ -1,2 +0,0 @@
-# work-portfolio
-Bernardo's work portfolio
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Bernardo Aguayo Portfolio',
       template: './public/index.html',
     }),
   ],
