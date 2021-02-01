64baa5b03d47dd2f667068e79d0dc91fdeefa28f
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-31 19:17:56 -0600

    [add] add tests set up

---
+++ src/__tests__/components/app.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+import { render } from '@testing-library/react';
+import App from '../../App';
+
+it('renders without crashing', () => {
+  render(<App />);
+});
