49d72cfa5a24ff24a9f21a1702a01f05fa3a036e
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-31 21:41:49 -0600

    [update] fix bugs with typescript config

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
 
 const root = document.getElementById('root');
 
-render(
-	<>
-		<h1>Im alive</h1>
-		<App />
-	</>,
-	root,
-);
+render(<h1>hello</h1>, root);
