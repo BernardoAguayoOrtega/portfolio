cfb961431de9aacf71cb5ce82f6b2bcdc1c67962
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-02-01 21:00:02 -0600

    [update] create intialView component add styles for intialview component add roboto font for glbal fonts

--- src/components/InitialView/index.tsx
+++ src/components/InitialView/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { FaDev } from 'react-icons/fa';
+import './styles.css';
 
 const App: React.FC = () => {
-  return <div className="container">inital view</div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <i>
+          <FaDev className="dev" />
+        </i>
+        <h2>Bernardo Aguayo</h2>
+        <p>changing text</p>
+        <button>button</button>
+      </div>
+    </div>
+  );
 };
 export default App;
