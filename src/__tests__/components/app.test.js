da6ef89a2f39519245d3ec335bcbbf59851db556
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-02-01 19:53:42 -0600

    [refactor] refactor files structure

--- src/__tests__/components/app.test.js
+++ src/__tests__/components/app.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-undef */
 import { render } from '@testing-library/react';
-import App from '../../App';
+import App from '../../components/App/index';
 
 it('renders without crashing', () => {
   render(<App />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './styles.css';
 
 const root = document.getElementById('root');
 
