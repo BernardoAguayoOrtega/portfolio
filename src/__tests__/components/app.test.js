dce9e77ffafb2d64fd40c9669bda2231f127953b
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-02-09 00:04:51 -0600

    [fix] fix bugs with set up of testing

--- src/__tests__/components/app.test.js
+++ src/__tests__/components/app.test.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-undef */
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../../components/App/index';
 
 it('renders without crashing', () => {
   render(<App />);
+
+  expect(screen.queryByText(/the name is required/i)).not.toBeInTheDocument();
 });
